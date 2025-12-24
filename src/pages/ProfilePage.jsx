5320f2442667a977148efaa5228dd2176feb2da4
Tan Boon Xuan <boonxuan05@gmail.com>
2025-12-24 18:29:34 +0800

    Update progress

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,7 +1,9 @@
-import { Navbar, Container, Button, NavbarCollapse } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 import useLocalStorage from "use-local-storage";
+import ProfileSideBar from "../components/ProfileSideBar";
+import ProfileMidBody from "../components/ProfileMidBody";
 
 
 export default function ProfilePage() {
@@ -21,25 +23,11 @@ export default function ProfilePage() {
 
     return (
         <>
-            <Navbar bg="light">
-                <Container>
-                    <Navbar.Brand href="/">
-                        <i 
-                            className="bi bi-twitter"
-                            style={{ fontSize: 30, color: 'dodgerblue' }}
-                        >
-                        </i>
-                    </Navbar.Brand>
-                    <NavbarCollapse className="justify-content-end">
-                        <Button variant="primary" onClick={handleLogout}>
-                            Logout
-                        </Button>
-                    </NavbarCollapse>
-                </Container>
-            </Navbar>
-
-            <Container className="mt-3">
-                <h2>Your profile</h2>
+            <Container>
+                <Row>
+                    <ProfileSideBar handleLogout={handleLogout} />
+                    <ProfileMidBody />
+                </Row>
             </Container>
         </>
     )
